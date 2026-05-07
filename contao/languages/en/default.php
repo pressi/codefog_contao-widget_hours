9d7f3783ca4cfbd5cc6a04b8c31e6ed23558db13
Stephan Preßl <mail@stephanpressl.at>
2026-05-07 10:44:58 +0200

    Update default.php
    add en translation

--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -3,3 +3,6 @@
 // Miscellaneous
 $GLOBALS['TL_LANG']['MSC']['wh_from'] = 'From';
 $GLOBALS['TL_LANG']['MSC']['wh_to'] = 'To';
+$GLOBALS['TL_LANG']['MSC']['wh_closed_label'] = 'Closed';
+$GLOBALS['TL_LANG']['MSC']['wh_inherit_label'] = 'Inherit Time';
+$GLOBALS['TL_LANG']['MSC']['wh_day_before'] = 'day before';
