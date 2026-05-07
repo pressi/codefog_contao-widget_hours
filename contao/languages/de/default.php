d8ecb072dc4dd58ac37a84c49dbc65710c622f32
Stephan Preßl <mail@stephanpressl.at>
2026-05-07 10:43:49 +0200

    Update default.php
    add footer labels

--- contao/languages/de/default.php
+++ contao/languages/de/default.php
@@ -3,3 +3,6 @@
 // Miscellaneous
 $GLOBALS['TL_LANG']['MSC']['wh_from'] = 'Von';
 $GLOBALS['TL_LANG']['MSC']['wh_to'] = 'Bis';
+$GLOBALS['TL_LANG']['MSC']['wh_cloed_label'] = 'Geschlossen';
+$GLOBALS['TL_LANG']['MSC']['wh_inherit_label'] = 'Zeit erben';
+$GLOBALS['TL_LANG']['MSC']['wh_day_before'] = 'Tag davor';
